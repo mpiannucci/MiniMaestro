fd39b78d5f26d2cb95583359febd98a7cb97a2e7
miannucci <miannucci@navatekltd.com>
2015-04-20 14:22:56 -0400

    Better documentation.
    Added sublime text project file

--- maestro.py
+++ maestro.py
@@ -65,29 +65,30 @@ class Maestro:
 
     def write(self, *data):
         """ Write a message to the command port
-
         Handles writing the commands to the Maestro device
+
+        @param[in] data A list of commands to write to the device
+        @return True if the write was successful, False otherwise
 
         """
         if not self._commandPort.isOpen() or not self._commandPort.writable():
             print "Serial port closed. Please open the serial port!"
-            return 
+            return False
         for message in data: 
             self._commandPort.write(chr(message))
         self._commandPort.flush()
+        return True
 
     def setTarget(self, channel, target):
         """Sends a set target command to the controller
-
-        Input Arguments:
-        channel -- The channel to control by target (integer)
-        target  -- The integer value to trasmit as pulse width in units of quarter-microseconds
-
         The target is a non-negative integer. If the channel is configured as a servo,
         then the target represents the pulse width to transmit in units of quarter-microseconds.
         A target value of 0 tells the Maestro to stop sending pulses to the servo. If the channel
         is configured as a digital output, values less than 6000 tell the Maestro to drive the line
         low, while values of 6000 or greater tell the Maestro to drive the line high.
+
+        @param[in] channel The channel to control by target (integer)
+        @param[in] target The integer value to trasmit as pulse width in units of quarter-microseconds
 
         """
         outStr = [self._SET_TARGET_CMD, channel, self.LowBits(target), self.HighBits(target)]
@@ -96,14 +97,12 @@ class Maestro:
     def setTargetPercent(self, channel, percent):
         """Sends a set target command to the controller when given a percentage instead of an 
            explicit command
-
-        Input Arguments:
-        channel -- The channel to control by target (integer)
-        percent -- The integer value from -100 to 100 to transmit as a pulse width 
-
         The target is a percentage value between 0 and 100. If the percentage is negative the 
         corresponding motion will be in the negative direction (ccw). This function abstracts the
         normal set target command. 
+
+        @param[in] channel The channel to control by target (integer)
+        @param[in] percent The integer value from -100 to 100 to transmit as a pulse width
 
         """
         target = self.PercentToCommand(percent)
@@ -113,17 +112,15 @@ class Maestro:
     def setMultipleTargets(self, numTargets, channel1, target1, channel2,
             target2, channel3=None, target3=None, channel4=None, target4=None):
         """Set multiple servo targets at one time
-
-        Input Arguments:
-        numChannels -- The number of channels to be changed with command (min=2, max=4)
-        channel(i)  -- The channel to control by target (integer)
-        target(i)   -- The integer value to trasmit as pulse width in units of (0.25 microseconds)
-
         This command simultaneously sets the targets for a contiguous block of channels.
         The first byte specifies how many channels are in the contiguous block; this is the number
         of target values you will need to send. The second byte specifies the lowest channel
         number in the block. The subsequent bytes contain the target values for each of the channels,
         in order by channel number, in the same format as the Set Target command above.
+
+        @param[in] numChannels The number of channels to be changed with command (min=2, max=4)
+        @param[in] channel(i) The channel to control by target (integer)
+        @param[in] target[i] The integer value to trasmit as pulse width in units of (0.25 microseconds)
 
         """
         if (numTargets < 2) or (numTargets > 4):
@@ -139,11 +136,6 @@ class Maestro:
 
     def setSpeed(self, channel, speed):
         """Sends the speed command to the maestro channel
-
-        Input Arguments:
-        channel -- The channel of which to set the speed
-        speed   -- The speed limit of the rate of output value change in units of (0.25 microseconds)/(10 ms).
-
         For a speed value of 140, if you send a Set Target command to adjust the
         target from, say, 1000 microseconds to 1350 microseconds, it will take 100 ms to make that adjustment.
         A speed of 0 makes the speed unlimited, so that setting. the target will immediately
@@ -151,18 +143,15 @@ class Maestro:
         by the design of the servo itself, the supply voltage, and mechanical loads;
         this parameter will not help your servo go faster than what it is physically capable of.
 
+        @param[in] channel The channel of which to set the speed
+        @param[in] speed The speed limit of the rate of output value change in units of (0.25 microseconds)/(10 ms).
+
         """
         outStr = [self._SET_SPEED_CMD, channel, self.LowBits(speed), self.HighBits(speed)]
         self.write(*outStr)
 
     def setAcceleration(self, channel, acceleration):
         """Sets the acceleration limit of the servo channel
-
-        Input Arguments:
-        channel -- The channel of which to set the speed
-        acceleration -- The acceleration limit of the channels
-                        output in units of (0.25 microseconds)/(10 ms)/(80 ms)
-
         This command limits the acceleration of a servo channel’s output. The acceleration limit is a value
         from 0 to 255 in units of (0.25 microseconds)/(10 ms)/(80 ms), except in special cases. A value of 0 corresponds
         to no acceleration limit. An acceleration limit causes the speed of a servo to slowly ramp up until it
@@ -173,17 +162,15 @@ class Maestro:
         target of 1 ms to a target of 2 ms. The acceleration setting has no effect on channels configured
         as inputs or digital outputs.
 
+        @param[in] channel The channel of which to set the speed
+        @param[in] acceleration The acceleration limit of the channels output in units of (0.25 microseconds)/(10 ms)/(80 ms)
+
         """
         outStr = [self._SET_ACCELERATION_CMD, channel, self.LowBits(acceleration), self.HighBits(acceleration)]
         self.write(*outStr)
 
     def setPWM(self, onTime, period):
         """Sets the PWM to the specified onTime and period
-
-        Input Arguments:
-        On Time -- On time value in units of 1/48 microseconds
-        period  -- Period value in units of 1/48 microseconds
-
         This command sets the PWM output to the specified on time and period, in units of 1/48 microseconds.
         A period of 4800, for example, will generate a frequency of 10 kHz. The resolution on these
         values depends on the period as shown in the table below:
@@ -199,16 +186,15 @@ class Maestro:
         provide the best possible resolution with 100% and 0% duty cycle options, so you should use one of these
         periods if possible.
 
+        @param[in] onTime On time value in units of 1/48 microseconds
+        @param[in] period Period value in units of 1/48 microseconds
+
         """
         outStr = [self._SET_PWM_CMD, self.LowBits(onTime), self.HighBits(onTime), self.LowBits(period), self.HighBits(period)]
         self.write(*outStr)
 
     def getPosition(self, channel):
         """Returns the position of the given channel
-
-        Input Arguments:
-        channel -- The channel of which to get the position
-
         This command allows the device communicating with the Maestro to get the position value
         of a channel. If the specified channel is configured as a servo, this position value
         represents the current pulse width that the Maestro is transmitting on the channel,
@@ -223,6 +209,8 @@ class Maestro:
         NOTE: The position value returned by this command is equal to four times the number
               displayed in the Position box in the Status tab of the Maestro Control Center.
 
+        @param[in] channel The channel of which to get the position
+
         """
         self.write(self._GET_POSITION_CMD, channel)
         # Get the return bytes
@@ -232,13 +220,14 @@ class Maestro:
         return position
 
     def getMovingState(self):
-        """Returns a 0 if no servos are moving, or a 1 if servos are moving
-
+        """Get the current motion state of the servo
         This command is used to determine whether the servo outputs have reached their
         targets or are still changing, limited by speed or acceleration settings.
         Using this command together with the Set Target command, you can initiate several
         servo movements and wait for all the movements to finish before moving on to the
         next step of your program.
+
+        @return 1 if moving, 0 if not in motion
 
         """
         self.write(self._GET_MOVING_STATE_CMD)
@@ -247,7 +236,6 @@ class Maestro:
 
     def goHome(self):
         """Returns all servos back to home position
-
         This command sends all servos and outputs to their home positions, just
         as if an error had occurred. For servos and outputs set to “Ignore”, the
         position will be unchanged.
@@ -288,6 +276,8 @@ class Maestro:
                                                 instruction to be executed) to go out of bounds. This can happen if your program is not terminated by a quit,
                                                 return, or infinite loop.
 
+        @return The error code read from the servo
+
         """
         self.write(self._GET_ERRORS_CMD)
         # Get the response bytes
@@ -306,11 +296,8 @@ class Maestro:
     def PercentToCommand(self, percent):
         """Process the percent value given for the servo control
 
-        Input Arguments:
-        percent -- an integer value between -100 and 100.
-
-        Returns:
-        target  -- the equivalent value to send the servo a desired pulse-width
+        @param[in] percent An integer value between -100 and 100.
+        @return the equivalent value to send the servo a desired pulse-width
 
         """
         shift = (percent/100)*self.MAX_FROM_HOME
